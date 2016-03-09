0bcae19eb3279eb3932e8f22e572bafcb500607c
irahopkinson <ira_hopkinson@sil.org>
2016-03-09 13:24:48 +0700

    hotfix/copyJp wip: Add project clone script

---
+++ src/libraries/shared/scripts/migration/CopySfJamaicanPsalmsProject.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace libraries\shared\scripts\migration;
+
+use models\commands\ProjectCommands;
+use models\mapper\Id;
+use models\ProjectModel;
+use models\QuestionModel;
+use models\TextListModel;
+use models\TextModel;
+require_once APPPATH . 'models/TextModel.php';
+
+class CopySfJamaicanPsalmsProject
+{
+    public function run($mode = 'test')
+    {
+        $testMode = ($mode != 'run');
+        $message = "Copy SF Jamaican Psalms Project\n\n";
+
+        $projectCodeJP = 'jamaican_psalms';
+        $project = $this->getProjectByCode($projectCodeJP);
+        if (!$project) {
+            $message .= "Project to copy doesn't exist.\n";
+            return $message;
+        }
+
+        $projectCodeCopy = $projectCodeJP . '_copy';
+        if (ProjectCommands::projectCodeExists($projectCodeCopy)) {
+            $message .= "Copied Project already exists.\n";
+            return $message;
+        }
+
+        $projectCopy = $project;
+        $projectCopy->id = new Id();
+        $projectCopy->projectCode = $projectCodeCopy;
+        $projectCopy->projectName .= ' (copy)';
+        if (!$testMode) {
+            $message .= "  Saving changes to project $projectCopy->projectName.\n\n";
+            $projectCopy->write();
+        }
+
+        $textsExamined = 0;
+        $textsUpdated = 0;
+        $textList = new TextListModel($project);
+        $textList->read();
+
+        // foreach text in project
+        foreach ($textList->entries as $textParams) {
+            $textsExamined++;
+            $textId = $textParams['id'];
+            $text = new TextModel($project, $textId);
+            $textCopy = new TextModel($projectCopy);
+            $textCopy->title = $text->title;
+            $textCopy->audioFileName = $text->audioFileName;
+            $textCopy->content = $text->content;
+            $textCopy->isArchived = $text->isArchived;
+
+            $questionsList = $text->listQuestions();
+            foreach ($questionsList->entries as $questionParams) {
+                $questionId = $questionParams['id'];
+                $question = new QuestionModel($project, $questionId);
+                $questionCopy = new QuestionModel($projectCopy);
+                foreach ($question as $field => $value) {
+                    if ($field != 'id' || $field != 'answers') {
+                        $questionCopy->{$field} = $question->{$field};
+                    }
+                }
+
+                // copy Answers
+
+                if (!$testMode) {
+                    $questionCopy->write();
+                }
+            }
+
+            if (!$testMode) {
+                $textCopy->write();
+                $textsUpdated++;
+            }
+            $message .= "Copied text: $text->title\n";
+        }
+
+        // Copy Comments
+
+        // Copy Assets
+
+        return $message;
+    }
+
+    /**
+     *
+     * @param string $code
+     * @return ProjectModel
+     */
+    private function getProjectByCode($code)
+    {
+        $project = new ProjectModel();
+        if ($project->readByProperties(array('projectCode' => $code))) {
+            return ProjectModel::getById($project->id->asString());
+        } else {
+            return null;
+        }
+    }
+}
